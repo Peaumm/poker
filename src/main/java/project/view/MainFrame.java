ad7d303aff56bd2b3632055478117d28ba93e717
Peaumm <maxence.fouquet@gmail.com>
2024-12-02 11:02:08 +0100

    feat(project): add 2 packages

---
+++ src/main/java/project/view/MainFrame.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        this.setTitle("Poker");
+        this.setSize(1280, 720 );
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        CardLayout cardLayout = new CardLayout();
+        JPanel mainPanel = new JPanel(cardLayout);
+
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
+        this.add(topPanel, BorderLayout.NORTH);
+
+        add(mainPanel);
+        cardLayout.show(mainPanel, "home");
+
+    }
+
+}
+
