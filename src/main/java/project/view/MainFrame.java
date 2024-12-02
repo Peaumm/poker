2833e800ab1a49f88a66ab4a538051b725c010b2
keilua <keilua.richard@gmail.com>
2024-12-02 12:42:32 +0100

    keilua

--- src/main/java/project/view/MainFrame.java
+++ src/main/java/project/view/MainFrame.java
@@ -1,5 +1,11 @@
-import javax.swing.*;
-import java.awt.*;
+package project.view;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
 
