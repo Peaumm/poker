307cec33a4f2cd775c2a91d9bfd42e21c2e57a61
JustWhxt <tibo.hrn@gmail.com>
2024-12-02 11:50:42 +0100

    Add properties Player

--- src/main/java/project/model/Player.java
+++ src/main/java/project/model/Player.java
@@ -1,4 +1,62 @@
 package project.model;
 
 public class Player {
+
+  private Integer id;
+  private String name;
+  private Integer cards;
+  private Integer money;
+
+
+  public Player(){};
+
+  public Player(Integer id, String name, Integer cards, Integer money) {
+    this.id = id;
+    this.name = name;
+    this.cards = cards;
+    this.money = money;
+  }
+
+  public Player(String name, Integer cards, Integer money) {
+    this.name = name;
+    this.cards = cards;
+    this.money = money;
+  }
+
+  public Player(Integer id, String name, Integer money) {
+    this.id = id;
+    this.name = name;
+    this.cards = money;
+  }
+
+  public Player(String name, Integer carte) {
+    this.name = name;
+    this.cards = carte;
+  }
+
+
+  public Integer getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+}
+
+  public Integer getcards() {
+    return cards;
+  }
+  
+  public Integer getMoney() {
+    return money;
+  }
+
+  public void setMoney(Integer money) {
+    this.money = money;
+  }
+
 }
--- src/main/java/project/view/MainFrame.java
+++ src/main/java/project/view/MainFrame.java
@@ -1,3 +1,5 @@
+package project.model;
+
 import javax.swing.*;
 import java.awt.*;
 
