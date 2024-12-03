925ab014a7ea03fad2d9f06b7fb165cecf266e03
Peaumm <maxence.fouquet@gmail.com>
2024-12-03 10:57:30 +0100

    fix(MainFrame)

--- src/main/java/project/view/MainFrame.java
+++ src/main/java/project/view/MainFrame.java
@@ -4,15 +4,6 @@ import project.view.cartes.ShowCards;
 
 import javax.swing.*;
 import java.awt.*;
-package project.view;
-
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
-
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-main
 
 public class MainFrame extends JFrame {
 
