a66425b0b03b5249ce26fbb777b6e2812dbd2501
JustWhxt <tibo.hrn@gmail.com>
2024-12-06 12:12:41 +0100

    Add method (mise x2) win for player

--- src/main/java/project/view/Input/MiseInputPanel.java
+++ src/main/java/project/view/Input/MiseInputPanel.java
@@ -4,9 +4,10 @@ import java.awt.*;
 import javax.swing.*;
 
 public class MiseInputPanel extends JPanel {
-  private JButton btnMiser1, btnMiser3, btnMiser5, btnMiser10, btnMiser50, btnMiser100, btnTapis;
+  private JButton btnMiser1, btnMiser3, btnMiser5, btnMiser10, btnMiser50, btnMiser100, btnTapis, btnWin;
   private JLabel resultLabel;
   private int playerMoney = 0;
+  private int lastBet = 0;
 
   public MiseInputPanel(int initialMoney) {
     this.playerMoney = initialMoney;
@@ -25,6 +26,7 @@ public class MiseInputPanel extends JPanel {
     btnMiser50 = createButton("Mise 50");
     btnMiser100 = createButton("Mise 100");
     btnTapis = createButton("Tapis");
+    btnWin = createButton("Gagner");
 
     // Ajouter les boutons au panneau
     buttonsPanel.add(btnMiser1);
@@ -34,6 +36,7 @@ public class MiseInputPanel extends JPanel {
     buttonsPanel.add(btnMiser50);
     buttonsPanel.add(btnMiser100);
     buttonsPanel.add(btnTapis);
+    buttonsPanel.add(btnWin);
 
     // Ajouter le panneau des boutons à droite
     add(buttonsPanel, BorderLayout.EAST);
@@ -59,14 +62,37 @@ public class MiseInputPanel extends JPanel {
     btnMiser50.addActionListener(e -> miser(50));
     btnMiser100.addActionListener(e -> miser(100));
     btnTapis.addActionListener(e -> miser(playerMoney));
+    btnWin.addActionListener(e -> playerWin());
   }
 
   private void miser(int amount) {
     if (amount > playerMoney) {
+      System.out.println("Tentative de miser " + amount + " jetons, mais pas assez de jetons !");
+      resultLabel.setForeground(Color.RED);
       resultLabel.setText("Erreur : Vous ne pouvez pas miser plus que votre somme disponible !");
     } else {
+      lastBet = amount;
       playerMoney -= amount;
+      System.out.println("Mise acceptée : " + amount + " jetons. Somme restante : " + playerMoney);
+      resultLabel.setForeground(Color.BLACK);
       resultLabel.setText("Vous avez misé " + amount + " jetons. Somme restante : " + playerMoney + " jetons");
     }
+  }
+
+  private void playerWin() {
+    System.out.println("Dernière mise : " + lastBet);
+    if (lastBet <= 0) {
+      System.out.println("Erreur : Aucun pari précédent pour gagner !");
+      resultLabel.setForeground(Color.RED);
+      resultLabel.setText("Erreur : Aucun pari précédent pour gagner !");
+      return;
+    }
+
+    int wins = lastBet * 2;
+    playerMoney += wins;
+    System.out.println("Vous avez gagné " + wins + " jetons !");
+    resultLabel.setText("Bravo ! Vous avez gagné " + wins + " jetons. Somme totale : " + playerMoney + " jetons");
+  
+    lastBet = 0;
   }
 }
--- src/main/java/project/view/cartes/ShowCards.java
+++ src/main/java/project/view/cartes/ShowCards.java
@@ -9,16 +9,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import project.tool.Functions;
+
 public class ShowCards extends JPanel {
 
     public int[][] tab = {
-        {1,2,3,4,5,6,7,8},
-        {9,10,11,12,13,14,15,16},
-        {17,18,19,20,21,22,23,24},
-        {25,26,27,28,29,30,31,32},
-        {33,34,35,36,37,38,39,40},
-        {41,42,43,44,45,46,47,48},
-        {49,50,51,52,0,0,0,0},
+            { 1, 2, 3, 4, 5, 6, 7, 8 },
+            { 9, 10, 11, 12, 13, 14, 15, 16 },
+            { 17, 18, 19, 20, 21, 22, 23, 24 },
+            { 25, 26, 27, 28, 29, 30, 31, 32 },
+            { 33, 34, 35, 36, 37, 38, 39, 40 },
+            { 41, 42, 43, 44, 45, 46, 47, 48 },
+            { 49, 50, 51, 52, 0, 0, 0, 0 },
     };
 
     JFrame frame;
@@ -31,11 +32,11 @@ public class ShowCards extends JPanel {
     MiseInputPanel mise;
     JButton button;
 
-    public ShowCards () {
+    public ShowCards() {
         frame = new JFrame();
         frame.setLayout(new BorderLayout());
         frame.setTitle("Poker");
-        frame.setSize(1280, 720 );
+        frame.setSize(1280, 720);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         cartePanel = new JPanel();
@@ -192,7 +193,7 @@ public class ShowCards extends JPanel {
             System.out.println();
         }
     }
-  
+
     public void positionCarte(int ligne, int colonne, int[][] tab) {
         tab[ligne][colonne] = 0;
     }
